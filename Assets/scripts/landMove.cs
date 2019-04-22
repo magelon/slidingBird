e118fdc3fa9ebb786862f5fd1f079121541799ea
magelon <alfardproject@gmail.com>
2019-04-22 10:33:52 -0700

    c

---
+++ Assets/scripts/landMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class landMove : MonoBehaviour
+{
+    public GameObject player;
+    private Transform pc;
+    public float distance;
+    private bool landReady;
+
+    void Update()
+    {
+        pc = player.transform;
+
+        if(pc.position.x - transform.position.x >= distance)
+        {
+            landReady = false;
+        }
+
+        if(pc.position.x - transform.position.x > distance&&!landReady)
+        {
+            this.transform.position = new Vector2(transform.position.x + 18, transform.position.y);
+            //draw land scape
+            GetComponent<waterShapes>().DrawTheland();
+            landReady = true;
+        }
+
+    }
+}
---
+++ Assets/scripts/playerMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right*20f);
+        }
+    }
+}
